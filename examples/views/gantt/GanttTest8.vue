fbc36613234bc3670536312ee20cb7c043d0691f
xuliangzhan <xu_liangzhan@163.com>
2026-01-06 19:34:11 +0800

    releases 4.2.3

--- examples/views/gantt/GanttTest8.vue
+++ examples/views/gantt/GanttTest8.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
-    <vxe-button status="success" @click="getPendingEvent">获取已标记数据</vxe-button>
-    <vxe-gantt ref="ganttRef" v-bind="ganttOptions">
-      <template #action="{ row }">
-        <vxe-button mode="text" status="error" @click="pendingRow(row, true)">标记</vxe-button>
-        <vxe-button mode="text" @click="pendingRow(row, false)">取消</vxe-button>
-      </template>
-    </vxe-gantt>
+    <div style="margin-bottom: 8px;">
+      <vxe-radio-group v-model="viewMode" @change="changeViewEvent">
+        <vxe-radio-button checked-value="1" content="年视图"></vxe-radio-button>
+        <vxe-radio-button checked-value="2" content="季度视图"></vxe-radio-button>
+        <vxe-radio-button checked-value="3" content="月视图"></vxe-radio-button>
+        <vxe-radio-button checked-value="4" content="周视图"></vxe-radio-button>
+        <vxe-radio-button checked-value="5" content="日视图"></vxe-radio-button>
+      </vxe-radio-group>
+    </div>
+
+    <vxe-gantt v-bind="ganttOptions"></vxe-gantt>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive } from 'vue'
-import { VxeUI } from '../../../packages'
-import { VxeGanttProps, VxeGanttInstance } from '../../../types'
+import { reactive, ref } from 'vue'
+import { VxeGanttProps, VxeGanttPropTypes } from '../../../types'
 
 interface RowVO {
   id: number
@@ -23,77 +26,98 @@ interface RowVO {
   progress: number
 }
 
-const ganttRef = ref<VxeGanttInstance<RowVO>>()
+const viewMode = ref('1')
+
+const taskViewConfig = reactive<VxeGanttPropTypes.TaskViewConfig>({
+  scales: ['year'], // 设置日期轴
+  viewStyle: {
+    // 自定义任务视图单元格样式
+    cellStyle ({ scaleType, row, dateObj }) {
+      if (row.progress >= 80) {
+        return {
+          backgroundColor: '#d9f5e3'
+        }
+      }
+      if (scaleType === 'date') {
+        // 周日高亮
+        if (dateObj.e === 0) {
+          return {
+            backgroundColor: '#daf9f5'
+          }
+        }
+      }
+    }
+  }
+})
 
 const ganttOptions = reactive<VxeGanttProps<RowVO>>({
   border: true,
-  showOverflow: true,
-  keepSource: true,
-  height: 500,
+  height: 800,
+  // 自定义左侧表格行样式
+  rowStyle ({ row }) {
+    if (row.progress >= 80) {
+      return {
+        backgroundColor: '#d9f5e3'
+      }
+    }
+  },
   taskBarConfig: {
-    showProgress: true, // 是否显示进度条
-    showContent: true, // 是否在任务条显示内容
+    showProgress: true, // 是否显示进度
+    showTooltip: true, // 是否显示任务提示,鼠标移动到任务上面显示
     moveable: true, // 是否允许拖拽任务移动日期
     barStyle: {
-      round: true, // 圆角
-      bgColor: '#fca60b', // 任务条的背景颜色
-      completedBgColor: '#65c16f' // 已完成部分任务条的背景颜色
+      round: true,
+      bgColor: '#fca60b',
+      completedBgColor: '#65c16f'
     }
   },
-  taskViewConfig: {
-    tableStyle: {
-      width: 480 // 表格宽度
+  taskViewConfig,
+  taskBarTooltipConfig: {
+    // 自定义任务提示内容,也可以使用插槽模板
+    contentMethod ({ row }) {
+      return `${row.title}\n开始时间：${row.start}\n结束：${row.end}\n进度：${row.progress}`
     }
-  },
-  editConfig: {
-    trigger: 'dblclick',
-    mode: 'cell',
-    showStatus: true
-  },
-  keyboardConfig: {
-    isEdit: true, // 是否开启任意键进入编辑（功能键除外）
-    isDel: true, // 是否开启删除键功能
-    isEsc: true // 是否开启Esc键关闭编辑功能
   },
   columns: [
-    { type: 'seq', width: 70 },
-    { field: 'title', title: '任务名称', minWidth: 160, editRender: { name: 'VxeInput' } },
-    { field: 'start', title: '开始时间', width: 120, editRender: { name: 'VxeDatePicker' } },
-    { field: 'end', title: '结束时间', width: 120, editRender: { name: 'VxeDatePicker' } },
-    { field: 'progress', title: '进度(%)', width: 140, editRender: { name: 'VxeNumberInput' } },
-    { field: 'action', title: '操作', fixed: 'right', width: 140, slots: { default: 'action' } }
+    { field: 'title', title: '任务名称' },
+    { field: 'start', title: '开始时间', width: 100 },
+    { field: 'end', title: '结束时间', width: 100 }
   ],
   data: [
-    { id: 10001, title: '任务1', start: '2024-03-01', end: '2024-03-04', progress: 3 },
-    { id: 10002, title: '任务2', start: '2024-03-03', end: '2024-03-08', progress: 10 },
-    { id: 10003, title: '任务3', start: '2024-03-03', end: '2024-03-11', progress: 90 },
-    { id: 10004, title: '任务4', start: '2024-03-05', end: '2024-03-11', progress: 15 },
-    { id: 10005, title: '任务5', start: '2024-03-08', end: '2024-03-15', progress: 100 },
-    { id: 10006, title: '任务6', start: '2024-03-10', end: '2024-03-21', progress: 5 },
-    { id: 10007, title: '任务7', start: '2024-03-15', end: '2024-03-24', progress: 70 },
-    { id: 10008, title: '任务8', start: '2024-03-05', end: '2024-03-15', progress: 50 },
-    { id: 10009, title: '任务9', start: '2024-03-19', end: '2024-03-20', progress: 5 },
-    { id: 10010, title: '任务10', start: '2024-03-12', end: '2024-03-20', progress: 10 },
-    { id: 10011, title: '任务11', start: '2024-03-01', end: '2024-03-08', progress: 90 },
-    { id: 10012, title: '任务12', start: '2024-03-03', end: '2024-03-06', progress: 60 },
-    { id: 10013, title: '任务13', start: '2024-03-02', end: '2024-03-05', progress: 50 },
-    { id: 10014, title: '任务14', start: '2024-03-04', end: '2024-03-15', progress: 0 },
-    { id: 10015, title: '任务15', start: '2024-03-01', end: '2024-03-05', progress: 30 }
+    { id: 10001, title: '任务1', start: '2024-08-10', end: '2024-11-21', progress: 36 },
+    { id: 10002, title: '任务2', start: '2024-01-03', end: '2024-03-08', progress: 49 },
+    { id: 10003, title: '任务3', start: '2024-05-03', end: '2024-07-11', progress: 80 },
+    { id: 10004, title: '任务4', start: '2024-04-05', end: '2024-06-11', progress: 68 },
+    { id: 10005, title: '任务5', start: '2024-07-08', end: '2024-08-15', progress: 100 },
+    { id: 10006, title: '任务6', start: '2023-08-01', end: '2023-11-15', progress: 48 },
+    { id: 10007, title: '任务7', start: '2024-03-15', end: '2024-08-24', progress: 70 },
+    { id: 10008, title: '任务8', start: '2024-03-20', end: '2024-04-05', progress: 50 },
+    { id: 10009, title: '任务9', start: '2025-01-19', end: '2025-03-18', progress: 20 },
+    { id: 10010, title: '任务10', start: '2024-12-12', end: '2025-02-10', progress: 72 },
+    { id: 10011, title: '任务11', start: '2025-01-10', end: '2025-02-25', progress: 50 },
+    { id: 10012, title: '任务12', start: '2025-10-05', end: '2025-12-20', progress: 20 },
+    { id: 10013, title: '任务13', start: '2023-01-05', end: '2023-02-01', progress: 20 },
+    { id: 10014, title: '任务14', start: '2023-02-12', end: '2023-02-27', progress: 20 }
   ]
 })
 
-const pendingRow = async (row: RowVO, status: boolean) => {
-  const $gantt = ganttRef.value
-  if ($gantt) {
-    $gantt.setPendingRow(row, status)
-  }
-}
-
-const getPendingEvent = () => {
-  const $gantt = ganttRef.value
-  if ($gantt) {
-    const pendingRecords = $gantt.getPendingRecords()
-    VxeUI.modal.alert(`标记：${pendingRecords.length} 行`)
+const changeViewEvent = () => {
+  switch (viewMode.value) {
+    case '1':
+      taskViewConfig.scales = ['year']
+      break
+    case '2':
+      taskViewConfig.scales = ['year', 'quarter']
+      break
+    case '3':
+      taskViewConfig.scales = ['year', 'month']
+      break
+    case '4':
+      taskViewConfig.scales = ['month', 'week']
+      break
+    case '5':
+      taskViewConfig.scales = ['month', 'day', 'date']
+      break
   }
 }
 </script>
