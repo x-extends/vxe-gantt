c158b5349f8b6dc61c115d0adc8150e262180c09
xuliangzhan <xu_liangzhan@163.com>
2026-07-08 10:08:14 +0800

    releases 4.5.4

--- types/all.d.ts
+++ types/all.d.ts
@@ -1,5 +1,7 @@
 import { App } from 'vue'
-import { VxeUIExport, VxeGlobalConfig } from 'vxe-pc-ui'
+import { VxeUIExport, VxeGlobalConfig, VxeComponentKebabCaseKeys } from 'vxe-pc-ui'
+
+import VxeGantt from 'vxe-pc-ui/types/components/gantt'
 
 declare global {
   interface Window {
@@ -8,6 +10,21 @@ declare global {
 }
 
 export function install (app: App, options?: VxeGlobalConfig): void
+
+interface AllComponents {
+  /**
+   * Gantt 甘特图
+   */
+  VxeGantt: typeof VxeGantt
+}
+
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents extends AllComponents {}
+}
+
+declare module '@vxe-ui/core' {
+  export interface VxeGlobalComponents extends AllComponents, VxeComponentKebabCaseKeys<AllComponents> {}
+}
 
 // Vxe core
 export * from 'vxe-pc-ui/types/ui'
