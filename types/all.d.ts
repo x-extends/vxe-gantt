0e79fa1694e7ce5a6699cd42332611c3d11038af
xuliangzhan <xu_liangzhan@163.com>
2026-07-08 10:08:04 +0800

    releases 3.5.4

--- types/all.d.ts
+++ types/all.d.ts
@@ -1,5 +1,7 @@
 import { VueConstructor } from 'vue'
-import { VxeUIExport, VxeGlobalConfig } from 'vxe-pc-ui'
+import { VxeUIExport, VxeGlobalConfig, VxeComponentKebabCaseKeys } from 'vxe-pc-ui'
+
+import VxeGantt from 'vxe-pc-ui/types/components/gantt'
 
 declare global {
   interface Window {
@@ -8,6 +10,17 @@ declare global {
 }
 
 export function install (app: VueConstructor, options?: VxeGlobalConfig): void
+
+interface AllComponents {
+  /**
+   * Gantt 甘特图
+   */
+  VxeGantt: typeof VxeGantt
+}
+
+declare module '@vxe-ui/core' {
+  export interface VxeGlobalComponents extends AllComponents, VxeComponentKebabCaseKeys<AllComponents> {}
+}
 
 // Vxe core
 export * from 'vxe-pc-ui/types/ui'
